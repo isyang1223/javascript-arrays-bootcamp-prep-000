196ed4fe233b2fc23455e82b596bd6e024caac2a
isyang1223 <isyang1223@gmail.com>
2017-09-26 01:46:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,4 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element){
-  chocolateBars.unshift(element);
-  return array;
+  return [element, ...array]  
 }
-//
-// function destructivelyAddElementToBeginningOfArray(array, element){
-//   chocolateBars.push(element);
-//   chocolateBars;
-// }
