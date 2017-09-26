fb784ac29203b233fea36e9178cbb240887e0f81
isyang1223 <isyang1223@gmail.com>
2017-09-26 01:52:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,5 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 function addElementToEndOfArray(array, element){
-  return [array, ...element]
+  return [...array, element]
 }
